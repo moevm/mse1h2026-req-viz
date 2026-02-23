ade4d6f116896934b4a911d64485b796b7fae380
w3hhh-m <w3hhh.m@gmail.com>
2026-02-23 19:42:39 +0300

    add more exceptions for neo4j service

--- graph/exceptions.py
+++ graph/exceptions.py
@@ -8,3 +8,19 @@ class GraphConnectionError(GraphError):
 
 class QueryError(GraphError):
     """Ошибка выполнения запроса к Neo4j."""
+
+
+class NodeNotFoundError(GraphError):
+    """Узел не найден в Neo4j."""
+
+
+class RelationshipNotFoundError(GraphError):
+    """Связь не найдена в Neo4j."""
+
+
+class DuplicateNodeError(GraphError):
+    """Узел уже существует в Neo4j."""
+
+
+class InvalidFilterError(GraphError):
+    """Неправильные параметры для фильтрации."""
