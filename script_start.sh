6dd6d07b83e43b20630e3d3f9f5828a7e9b6705d
Polina Kurkina <Polina.Kurkina@oktetlabs.ru>
2026-02-24 16:23:44 +0300

    sript: fix

--- script_start.sh
+++ script_start.sh
@@ -2,4 +2,4 @@
 # Start the FastAPI development server for the Ecosystem Analyzer module
 # The server will automatically reload on code changes (--reload flag)
 # Runs on port 8000 (http://localhost:8000)
-uvicorn src.ecosystem_analyzer.main:app --reload --port 8000
+uvicorn ecosystem_analyzer.main:app --reload --port 8000
