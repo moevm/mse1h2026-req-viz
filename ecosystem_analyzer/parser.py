85d0f3ae1d20e2ed7aa414f2728239b8c961d6e6
Polina Kurkina <Polina.Kurkina@oktetlabs.ru>
2026-04-27 11:05:52 +0300

    delete mock_graph

--- ecosystem_analyzer/parser.py
+++ ecosystem_analyzer/parser.py
@@ -60,34 +60,3 @@ class ParserWrapper:
                 truncated=False,
             ),
         )
-
-
-MOCK_GRAPH = GraphResponse(
-    nodes=[
-        {"id": "tech_001", "label": "Apache Kafka", "type": "Technology"},
-        {"id": "tech_002", "label": "RabbitMQ", "type": "Technology"},
-        {"id": "comp_001", "label": "Confluent", "type": "Company"},
-        {"id": "lic_001", "label": "Apache 2.0", "type": "License"},
-    ],
-    edges=[
-        {
-            "source": "tech_001",
-            "target": "tech_002",
-            "type": "ALTERNATIVE_TO",
-            "weight": 0.9,
-        },
-        {
-            "source": "tech_001",
-            "target": "comp_001",
-            "type": "DEVELOPED_BY",
-            "weight": 1.0,
-        },
-        {
-            "source": "tech_001",
-            "target": "lic_001",
-            "type": "LICENSED_UNDER",
-            "weight": 1.0,
-        },
-    ],
-    statistics=Statistics(total_nodes=4, total_edges=3, max_depth=2),
-)
